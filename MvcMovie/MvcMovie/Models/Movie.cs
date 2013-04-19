554ba982ce0acc423569b70040315bf458a2c79e
William <willjlee@fake.email>
2013-04-18 22:46:19 -0400

    validation added. tutorial complete.

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Required]
         public string Genre { get; set; }
+
+        [Range(0,100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 
